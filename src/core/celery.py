a0554eadbde950a252dd071dfce8ebb86c09700c
Thibault Jouannic <thibault@miximum.fr>
2021-07-30 12:49:46 +0200

    Update celery conf

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,43 +1,17 @@
-try:
-    import simplejson as json
-except ImportError:
-    import json
-from datetime import date, datetime
-from decimal import Decimal
+import os
 
-from kombu.serialization import register
 from celery import Celery
 
-from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
+
+from django.conf import settings  # noqa
 
 
-app = Celery("phase")
-
-
-# We need a custom serializer to handle date and datetime objects.
-class JSONSerializer(json.JSONEncoder):
-    def default(self, data):
-        if isinstance(data, (date, datetime)):
-            return data.isoformat()
-        elif isinstance(data, Decimal):
-            return float(data)
-        raise TypeError("Unable to serialize %r (type: %s)" % (data, type(data)))
-
-
-def my_dumps(obj):
-    return json.dumps(obj, cls=JSONSerializer)
-
-
-register(
-    "betterjson",
-    my_dumps,
-    json.loads,
-    content_type="application/x-myjson",
-    content_encoding="utf-8",
-)
-
-
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object("django.conf:settings")
+app = Celery('phase')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def celery_debug_task(self):
+    print('This is a debug task to verify that Celery works')
